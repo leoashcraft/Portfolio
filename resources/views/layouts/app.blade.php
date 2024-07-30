af5b5eeaedb147e8474e16754b76b96dbd059890
Leo Ashcraft <leo@ashcraft.tech>
2024-07-30 02:21:03 -0500

    Removed background animation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,39 +95,6 @@ Test
     </div>
     <!-- App End -->
 
-
-    <!-- Background Line and Animation -->
-    <div class="bg-lines fixed inset-0 -z-1 md:max-xl:max-w-[45rem] xl:max-w-60rem 2xl:max-w-container mx-auto max-sm:px-8 sm:max-xl:px-12">
-        <div class="line-wrapper max-w-[1130px] w-full h-full ml-auto 2xl:-mr-24 relative flex justify-between *:w-px *:h-full *:border-r *:border-dashed *:border-slate-300 dark:*:border-metalBlack *:relative *:before:absolute *:before:bg-theme *:before:rotate-45 *:before:-left-1 *:before:w-2 *:before:h-2">
-            <div class="line1 before:animate-top_bottom"></div>
-            <div class="line2 before:bottom-0 before:animate-bottom_top before:animate-delay-3s"></div>
-            <div class="line3 before:animate-top_bottom before:animate-delay-3s"></div>
-            <div class="line4 before:bottom-0 before:animate-bottom_top before:animate-delay-2s"></div>
-        </div>
-        <div class='light x1'></div>
-        <div class='light x2'></div>
-        <div class='light x3'></div>
-        <div class='light x4'></div>
-        <div class='light x5'></div>
-        <div class='light x6'></div>
-        <div class='light x7'></div>
-        <div class='light x8'></div>
-        <div class='light x9'></div>
-    </div>
-
-    <div class="bg-lines fixed inset-0 -z-1">
-        <div class='light x1'></div>
-        <div class='light x2'></div>
-        <div class='light x3'></div>
-        <div class='light x4'></div>
-        <div class='light x5'></div>
-        <div class='light x6'></div>
-        <div class='light x7'></div>
-        <div class='light x8'></div>
-        <div class='light x9'></div>
-    </div>
-    <!-- Ends Here -->
-
     <!-- Style Switcher Started
     <div class="style-switcher fixed right-0 top-1/2 -translate-y-1/2 translate-x-[15rem] w-[15rem] [&.active]:translate-x-0 transition z-[9999]">
 

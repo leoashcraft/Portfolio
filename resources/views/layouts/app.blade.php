0fb133e31bb5fd13a80380d03ea280c7dd665d5b
Leo Ashcraft <leoashcraft@gmail.com>
2024-08-01 21:09:13 -0500

    Minimizing Cumulative Layout Shift

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <link rel="apple-touch-icon" sizes="180x180" href="{{ config('app.static_url') }}/icons/apple-touch-icon.png?v=1">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ config('app.static_url') }}/icons/favicon-32x32.png?v=1">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ config('app.static_url') }}/icons/favicon-16x16.png?v=1">
-        <link rel="manifest" href="{{ config('app.static_url') }}/icons/site.webmanifest?v=1">
+        <link rel="manifest" href="/icons/site.webmanifest?v=1">
         <link rel="mask-icon" href="{{ config('app.static_url') }}/icons/safari-pinned-tab.svg?v=1" color="#222222">
         <link rel="shortcut icon" href="{{ config('app.static_url') }}/icons/favicon.ico?v=1">
         <meta name="msapplication-TileColor" content="#222222">
@@ -36,7 +36,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
         <!-- All CSS Here
         ================================================== -->
-        <link rel="stylesheet" href="{{ config('app.static_url') }}/css/all.css">
+        <link rel="stylesheet" href="{{ config('app.static_url') }}/css/all.min.css">
     </head>
     <body class="relative custom_cursor">
         <!-- Theme Toggle Start -->
@@ -79,7 +79,7 @@
         </div>
         <!-- App End -->
         <!-- Js Library Start -->
-        <script src="{{ config('app.static_url') }}/js/all.js"></script>
+        <script src="{{ config('app.static_url') }}/js/all.min.js"></script>
         <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.key') }}" defer></script>
         <!-- Js Library End -->
     </body>

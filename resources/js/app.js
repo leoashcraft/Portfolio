69740d65da5c9af0a51329ccb0c14b512c2ab7f3
Leo Ashcraft <leo@ashcraft.tech>
2024-08-02 01:21:25 -0500

    Migrated from Laravel Mix to Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import '../css/swiper-bundle.min.css';
 import '../css/style.min.css';
 import '../css/custom-embedded.css';
 
-import './jquery-3.6.0.min.js';
+import 'jquery';
 import './waypoints.min.js';
 import './cd-headline.js';
 import './jquery.counterup.min.js';

63516bb0d785b026cf0ff4d84b41ab78892aff5f
Leo Ashcraft <leoashcraft@gmail.com>
2024-08-01 21:49:26 -0500

    Converted portfolio video to webm

--- public/js/all.js
+++ public/js/all.js
@@ -653,3 +653,31 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize the theme on page load
     initializeTheme();
 });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        let lazyVideos = [...document.querySelectorAll("video.lazy")]
+
+        if ("IntersectionObserver" in window) {
+        let lazyVideoObserver = new IntersectionObserver(function(entries) {
+            entries.forEach(function(video) {
+            if (video.isIntersecting) {
+                for (let source in video.target.children) {
+                let videoSource = video.target.children[source];
+                if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                    videoSource.src = videoSource.dataset.src;
+                }
+                }
+
+                video.target.load();
+                video.target.classList.remove("lazy");
+                lazyVideoObserver.unobserve(video.target);
+            }
+            });
+        });
+
+        lazyVideos.forEach(function(lazyVideo) {
+            lazyVideoObserver.observe(lazyVideo);
+        });
+        }
+    });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1039,12 +1039,11 @@
                 </div><!--./portfolio-card-->
                 <div class="item md:col-span-2 group">
                         <div class="img-wrapper">
-                            <img src="{{ config('app.static_url') }}/img/portfolio/amstel.gif"
-                                width="960px"
-                                height="540px"
-                                loading="lazy"
-                                class="rounded-lg max-md:h-[17rem] w-full max-md:object-cover max-md:object-center transition-all duration-300 group-hover:blur-xs"
-                                alt="portfolio">
+                            <video class="lazy" playsinline autoplay loop muted preload="metadata">
+                                <source data-src="{{ config('app.static_url') }}/img/portfolio/amstelplumbing.webm"
+                                    class="rounded-lg max-md:h-[17rem] w-full max-md:object-cover max-md:object-center transition-all duration-300 group-hover:blur-xs"
+                                    alt="portfolio">
+                            </video>
                             <div
                                 class="absolute inset-0 transition-all duration-300 opacity-0 overlay bg-gradient-to-t from-white dark:from-black to-transparent rounded-xl group-hover:opacity-100">
                             </div>

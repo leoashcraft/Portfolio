4770a398b0b39f4a6d798d4ea460db07bf07cbb3
Leo Ashcraft <leo@ashcraft.tech>
2026-01-07 21:33:33 -0600

    Fix remaining accessibility issues
    - Change alt to aria-label on desktop menu links
    - Fix typo Poftfolio -> Portfolio
    - Darken all theme colors for WCAG AA compliance:
      - Teal rgb(0 188 145) -> rgb(0 120 95) for 4.5:1 contrast
      - Light blue rgb(113 183 213) -> rgb(35 128 165)

--- resources/views/layouts/_menu_desktop.blade.php
+++ resources/views/layouts/_menu_desktop.blade.php
@@ -7,7 +7,7 @@
                 <a href="#home"
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="Home"
-                    alt="Home">
+                    aria-label="Home">
                     <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                         <i class="icon-home"></i>
                     </span>
@@ -17,7 +17,7 @@
                 <a href="#about"
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="About"
-                    alt="About">
+                    aria-label="About">
                     <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                         <i class="icon-user"></i>
                     </span>
@@ -27,7 +27,7 @@
                 <a href="#service"
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="Service"
-                    alt="Service">
+                    aria-label="Service">
                     <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                         <i class="icon-briefcase"></i>
                     </span>
@@ -37,7 +37,7 @@
                 <a href="#resume"
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="Resume"
-                    alt="Resume">
+                    aria-label="Resume">
                     <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                         <i class="icon-folder-open"></i>
                     </span>
@@ -46,8 +46,8 @@
             <li data-scroll-nav="4" class="group">
                 <a href="#portfolio"
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
-                    data-title="Poftfolio"
-                    alt="Poftfolio">
+                    data-title="Portfolio"
+                    aria-label="Portfolio">
                     <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                         <i class="icon-tasks"></i>
                     </span>
@@ -57,7 +57,7 @@
                 <a href="#contact"
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="Contact"
-                    alt="Contact">
+                    aria-label="Contact">
                     <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                         <i class="icon-mail-alt"></i>
                     </span>
@@ -67,7 +67,7 @@
             <a href="https://www.linkedin.com/in/leo3" 
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="LinkedIn"
-                    alt="LinkedIn"
+                    aria-label="LinkedIn"
                     target="_new">
                 <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                     <i class="icon-linkedin-squared"></i>
@@ -78,7 +78,7 @@
             <a href="https://github.com/leoashcraft" 
                     class="w-9 h-9 rounded-full flex-center group-[&.active]:bg-white dark:group-[&.active]:bg-metalBlack group-hover:bg-white dark:group-hover:bg-metalBlack transition-all duration-300 before:content-[attr(data-title)] before:absolute before:right-10 before:bg-white dark:before:bg-metalBlack before:text-sm text-theme dark:before:text-white before:px-4 before:py-2 before:rounded-md before:font-normal dark:before:font-light before:opacity-0 before:transition-all before:duration-200 group-hover:before:opacity-100 after:content-[''] after:absolute after:w-0 after:h-0 after:right-8 after:border-solid after:border-t-4 after:border-r-0 after:border-b-4 after:border-l-8 after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-white dark:after:border-l-metalBlack after:opacity-0 after:transition-all after:duration-200 group-hover:after:opacity-100"
                     data-title="GitHub"
-                    alt="GitHub"
+                    aria-label="GitHub"
                     target="_new">
                 <span class="text-black dark:text-white group-hover:text-theme group-[&.active]:text-theme">
                     <i class="icon-github-circled"></i>
